df47e41218b233ccf73b6cbe107daa2b85a4e9d4
Nick-Perelygin <77kalian77@gmail.com>
2023-02-23 18:36:40 +0200

    c

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body")
+const buttonChangeColor = document.querySelector(".change-color")
+const spanColor = document.querySelector(".color")
+
+
+buttonChangeColor.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  return spanColor.textContent = `${bodyEl.style.backgroundColor}`;
+})
