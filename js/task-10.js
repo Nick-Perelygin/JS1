313a5d2c5455e9560f9d6a93c3a3aecdd6dc98d3
Nick-Perelygin <77kalian77@gmail.com>
2023-02-24 23:13:22 +0200

    6 homework

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector("[type='number']")
+const buttonCreateEl = document.querySelector("[data-create]")
+const buttonDestroyEl = document.querySelector("[data-destroy]")
+const divBoxesEl = document.querySelector("#boxes")
+
+divBoxesEl.style.display = 'flex';
+divBoxesEl.style.flexWrap = 'wrap';
+
+function createBoxes(number) {
+  let divArray = [];
+  let sizeDefault = 30;
+  for (let i = 0; i < number; i += 1) {
+    sizeDefault += 10;
+    const newDivBoxesEl = document.createElement('div');
+    newDivBoxesEl.style.width = `${sizeDefault}px`;
+    newDivBoxesEl.style.height = `${sizeDefault}px`;
+    newDivBoxesEl.style.marginRight = `10px`;
+    newDivBoxesEl.style.backgroundColor = getRandomHexColor();
+    divArray.push(newDivBoxesEl);
+    divBoxesEl.appendChild(newDivBoxesEl);
+  }
+  return divBoxesEl.append(...divArray)
+}
+
+buttonCreateEl.addEventListener('click', () => {
+  if (Number(inputEl.value) >= 1 && Number(inputEl.value) <= 100) {
+    createBoxes(Number(inputEl.value))
+    inputEl.value = "";
+  } else {
+    inputEl.value = "";
+    return alert("Bведіть число від 1 до 100");
+  }
+})
+
+buttonDestroyEl.addEventListener('click', () => {
+divBoxesEl.innerHTML = "";
+})
+
